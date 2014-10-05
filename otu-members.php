0ae249e412e42111bf36ff2c9251a03a742f2ce8
cbdweb <nik@cbdweb.net>
2014-10-05 23:54:49 +1100

    login form widget;

--- otu-members.php
+++ otu-members.php
@@ -211,21 +211,5 @@ class Otu_login extends WP_Widget {
                 }
                 echo $after_widget;
 	}
-
-	function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-
-		return $instance;
-	}
-
-	function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
-		$title = strip_tags($instance['title']);
-?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
-<?php
-	}
 }
 
