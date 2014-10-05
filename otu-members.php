2839666a85e090cc9632ec913d7a3ea7aacd77c6
cbdweb <nik@cbdweb.net>
2014-10-06 02:06:31 +1100

    submit username with password appended;

--- otu-members.php
+++ otu-members.php
@@ -183,12 +183,12 @@ class Otu_login extends WP_Widget {
                     ?>
                     <h3 class="widget-title">Login</h3>
                     <form name="loginform-custom" id="loginform-custom" onSubmit="loginSubmit(this)" action="<?=site_url()?>/wp-login.php" method="post">
-                        <p class="login-username"><label for="user_login">surname / username</label>
+                        <p class="login-username"><label for="user_login">surname</label>
                             <input type="text" name="partial_log" id="user_login" class="input" size="20"/>
                             <input type="hidden" name="log" />
                         </p>
                         <P class="login-password">
-                            <label for="user_pass">Reg No / password</label>
+                            <label for="user_pass">Reg No</label>
                             <input type="password" name="pwd" id="user_pass" class="input" size="20"/>
                         </P>
                         <?php do_action( 'login_form' );?>
