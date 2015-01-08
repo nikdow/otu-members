216ae824267d9b284305ceefe3b887817f412018
cbdweb <nik@cbdweb.net>
2015-01-08 16:00:55 +1100

    remove file upload dir

--- memberlist.php
+++ memberlist.php
@@ -162,7 +162,6 @@ function otu_itemlist (  ) {
             }
             ?>
         </div>
-        <?=print_r(wp_upload_dir())?>
         <div id='items'>
             <div ng-show="membertype.length===0">
                 <h2>You have not selected any Member-types above</h2>
