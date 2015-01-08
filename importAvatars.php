9743d69c6abde8a12021770a8d7a562e5dc82de2
cbdweb <nik@cbdweb.net>
2015-01-08 20:39:27 +1100

    import avatars;

--- importAvatars.php
+++ importAvatars.php
@@ -10,23 +10,28 @@
         
     global $wpdb;
     $query = "SELECT * from avatar_temp";
-    $rows = $wpdb->get_rows( $query );
+    $rows = $wpdb->get_results( $query );
     foreach ($rows as $row ) {
         $file = $row->avatar;
         echo "file = " . $file . " ";
         $regno = $row->regno;
         echo "regno = " . $regno . " ";
         $query2 = "SELECT user_id from $wpdb->usermeta WHERE meta_value=" . $row->regno;
-        $row2 = $wpdb->get_rows( $query );
-        $user_id = $row2[0]->user_id;
+        $row2 = $wpdb->get_row( $query );
+        $user_id = $row2->user_id;
         echo "User ID = " . $user_id . " ";
         /* pretend the file was just uploaded */
         if ( preg_match ( "/.*\.(jpg|gif|png)$/", $file, $matches ) ) {
             $type = $mimes[$matches[1]];
-            $file = array('name'=>$file, 'tmp_name'=>$file, 'type'=>$type );
-            $avatar = wp_handle_upload( $file );
-            update_user_meta( $user_id, 'basic_user_avatar', array( 'full' => $avatar['url'] ) );
-            echo "success ";
+            $wp_upload_dir = wp_upload_dir();
+            $file = array('name'=>$file, 'tmp_name'=>$upload_path['basedir'] . '/' . $file, 'type'=>$type );
+            $avatar = wp_handle_upload( $file, array ( 'test_form' => false, 'test_size' => false, 'test_type'=>false, ) );
+            if ( empty( $avatar['file'] ) ) {
+				echo $avatar['error'] . " ";
+            } else {
+                update_user_meta( $user_id, 'basic_user_avatar', array( 'full' => $avatar['url'] ) );            
+                echo "success ";
+            }
         } else {
             echo " preg no match on file type ";
         }
