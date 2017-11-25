d32938c877fc1dc4bef18149f65ef63099feb54e
nik <nik@NikThinkpad>
2017-11-25 20:47:57 +1100

    diagnostic

--- memberlist.php
+++ memberlist.php
@@ -319,10 +319,11 @@ function get_items ( $query, $rows_per_page ) {
     
     $items = array();
     foreach ( $rows as $row ) {
+        error_log( print_r($row) );
         // get meta values directly, WP can be inefficient
         $queryc = $wpdb->prepare ( "SELECT meta_key, meta_value
              FROM $wpdb->usermeta
-             WHERE user_id='%u'", $row->ID );
+             WHERE user_id=%u", $row->ID );
         error_log( $queryc );
         $customs = $wpdb->get_results( $queryc );
         $custom = array();
